a9b9a07d82ba1662b82b9a25e675e7f69e1054ac
Brian Manoff <brian.a.manoff@gmail.com>
2022-05-23 08:54:21 -0700

    Finished join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -44,13 +44,98 @@ and e.gender = 'F'
 order by department_name asc;
 
 -- #4
-SELECT  t.title, count(*)
+SELECT  t.title, count(e.emp_no) as count
 FROM employees AS e
-JOIN titles AS t
-  ON t.emp_no = e.emp_no
-  Join departments as d
-  on d.dept_no = 
+JOIN dept_emp as de
+on de.emp_no = e.emp_no
+join departments as d
+on d.dept_no = de.dept_no
+join titles as t
+on t.emp_no = e.emp_no
 WHERE t.to_date >= NOW()
+and de.to_date >= NOW()
+and d.dept_no = 'd009'
 group by t.title;
 
-select * from departments;
+-- #5
+SELECT  d.dept_name as department_name, CONCAT(e.first_name, ' ', e.last_name) AS name, s.salary
+FROM employees AS e
+JOIN dept_emp AS de
+  ON de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+  join salaries as s
+  on s.emp_no = de.emp_no
+  join dept_manager as dm
+  on dm.emp_no = e.emp_no
+WHERE dm.to_date >= NOW()
+and s.to_date >= NOW()
+order by d.dept_name;
+
+-- #6
+
+SELECT  d.dept_no as dept_no, d.dept_name as department_name, count(e.emp_no) as num_employees
+FROM employees AS e
+JOIN dept_emp as de
+on de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+WHERE de.to_date >= NOW()
+group by department_name
+order by department_name asc;
+
+-- #7
+select d.dept_name as dept_name, avg(s.salary) as average_salary
+from salaries as s
+join dept_emp as de
+on de.emp_no = s.emp_no
+join departments as d
+on d.dept_no = de.dept_no
+where s.to_date >= NOW()
+group by dept_name
+order by average_salary desc
+limit 1;
+
+-- #8
+SELECT  e.first_name AS first_name, e.last_name as last_name
+FROM employees AS e
+join dept_emp as de
+on de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+  join salaries as s
+  on s.emp_no = e.emp_no
+  where d.dept_no = 'd001'
+  order by s.salary desc
+  limit 1;
+  
+ -- #9
+ SELECT  e.first_name AS first_name, e.last_name as last_name, s.salary as salary, d.dept_name as dept_name
+FROM employees AS e
+join dept_emp as de
+on de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+  join dept_manager as dm
+  on dm.emp_no = de.emp_no
+  join salaries as s
+  on s.emp_no = e.emp_no
+  where s.to_date >= NOW()
+  and dm.to_date >= NOW()
+  order by s.salary desc
+  limit 1;
+  
+  -- #10
+ SELECT d.dept_name as dept_name, round(avg(s.salary)) as average_salary
+FROM employees AS e
+join dept_emp as de
+on de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+  join salaries as s
+  on s.emp_no = e.emp_no
+  group by dept_name
+  order by average_salary desc;
+
+
+
